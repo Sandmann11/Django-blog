caa922a530027bde97dc64bdfdeaad910b5a8350
Arek P <sandmann@o2.pl>
2021-06-01 21:11:15 +0200

    .

--- blog/views.py
+++ blog/views.py
@@ -8,11 +8,11 @@ import pandas as pd
 import datetime as dt
 import plotly.graph_objects as go
 from binance.client import Client
-import bin_api
+from . import bin_api
 
 
 def main(request):
-    return render(request, 'blog/main.html', {})    
+    return render(request, 'blog/main.html', {})
 
 
 def admin(request):
@@ -24,31 +24,35 @@ def portfolio(request):
 
 
 def crypto(request):
-    json_data = requests.get('https://api.binance.com/api/v3/ticker/price').json()
+    json_data = requests.get(
+        'https://api.binance.com/api/v3/ticker/price').json()
     # df = pd.DataFrame(json_data)
 
     btc_price = round(float(json_data[11]['price']), 2)
-    eth_price  = round(float(json_data[12]['price']), 2)
+    eth_price = round(float(json_data[12]['price']), 2)
     xmr_price = round(float(json_data[480]['price']), 2)
 
     return render(request, 'blog/crypto.html', {
-        'btc_price':btc_price,
-        'eth_price':eth_price,
-        'xmr_price':xmr_price,
+        'btc_price': btc_price,
+        'eth_price': eth_price,
+        'xmr_price': xmr_price,
         # 'df':df
-        })
+    })
 
 
 def crypto_trades(request):
     trades_df = bin_api.latest_trades()
+    html_df = trades_df.to_html()
 
     return render(request, 'blog/crypto_trades.html', {
-        'trades_df':trades_df
-        })
+        'trades_df': trades_df,
+        'html_df': html_df,
+    })
 
 
 def crypto_all(request):
-    json_data = requests.get('https://api.binance.com/api/v3/ticker/price').json()
+    json_data = requests.get(
+        'https://api.binance.com/api/v3/ticker/price').json()
 
     btc_list = []
     eth_list = []
@@ -80,24 +84,26 @@ def crypto_all(request):
             usdt_list.append(coin)
 
     return render(request, 'blog/crypto_all.html', {
-        'btc_list':btc_list,
-        'eth_list':eth_list,
-        'bnb_list':bnb_list,
-        'eur_list':eur_list,
-        'usdt_list':usdt_list,
-        })
+        'btc_list': btc_list,
+        'eth_list': eth_list,
+        'bnb_list': bnb_list,
+        'eur_list': eur_list,
+        'usdt_list': usdt_list,
+    })
 
 
 def btc_chart(request):
     market = 'BTCUSDT'
     tick_interval = '1d'
 
-    url = 'https://api.binance.com/api/v3/klines?symbol='+market+'&interval='+tick_interval
+    url = 'https://api.binance.com/api/v3/klines?symbol=' + \
+        market+'&interval='+tick_interval
     data = requests.get(url).json()
 
     df = pd.DataFrame(data)
     df = df.drop(columns=[7, 8, 9, 10, 11])
-    df.columns = ['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time']
+    df.columns = ['open_time', 'open', 'high',
+                  'low', 'close', 'volume', 'close_time']
 
     df['open_time'] = [dt.datetime.fromtimestamp(i / 1000.0) for i in df['open_time']]
     df['close_time'] = [dt.datetime.fromtimestamp(i / 1000.0) for i in df['close_time']]
@@ -106,7 +112,7 @@ def btc_chart(request):
     df.index.name = 'date'
 
     fig = go.Figure(data=[go.Candlestick(
-                    x=df.index, 
+                    x=df.index,
                     open=df.open,
                     high=df.high,
                     low=df.low,
@@ -116,11 +122,12 @@ def btc_chart(request):
         title='BTC Historic Price Chart'
     )
 
-    chart = fig.to_html(full_html=False, default_height=800, default_width='100%')
+    chart = fig.to_html(full_html=False, default_height=800,
+                        default_width='100%')
 
     return render(request, 'blog/btc_chart.html', {
-        'chart':chart
-        })
+        'chart': chart
+    })
 
 
 class PostList(ListView):
